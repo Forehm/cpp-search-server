082f02cb61037c4691781443022c325dd40eef68
Egor <112934317+Forehm@users.noreply.github.com>
2023-07-10 22:56:50 +0300

    Update test_example_functions.cpp

--- search-server/test_example_functions.cpp
+++ search-server/test_example_functions.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 void AddDocument(SearchServer& search_server, int document_id, const std::string& document,
-    DocumentStatus status, const std::vector<int>& ratings) {
+    DocumentStatus status, const std::vector<int>& ratings)
+{
     search_server.AddDocument(document_id, document, status, ratings);
 }
