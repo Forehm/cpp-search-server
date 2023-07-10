f35674281ba5b96ea099fdd47c6d3b58a1ccb52f
Egor <112934317+Forehm@users.noreply.github.com>
2023-07-10 22:56:28 +0300

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -1,11 +1,14 @@
 #include "string_processing.h"
 
-std::vector<std::string> SplitIntoWords(const std::string& text) {
+std::vector<std::string> SplitIntoWords(const std::string& text) 
+{
     std::vector<std::string> words;
     std::string word;
-    for (const char c : text) {
+    for (const char c : text) 
+    {
         if (c == ' ') {
-            if (!word.empty()) {
+            if (!word.empty())
+            {
                 words.push_back(word);
                 word.clear();
             }
@@ -14,19 +17,22 @@ std::vector<std::string> SplitIntoWords(const std::string& text) {
             word += c;
         }
     }
-    if (!word.empty()) {
+    if (!word.empty()) 
+    {
         words.push_back(word);
     }
 
     return words;
 }
 
-std::vector<std::string_view> SplitIntoWordsView(std::string_view str) {
+std::vector<std::string_view> SplitIntoWordsView(std::string_view str) 
+{
     std::vector<std::string_view> result;
     str.remove_prefix(std::min(str.find_first_not_of(' '), str.size()));
     const std::string_view::size_type pos_end = std::string_view::npos;
 
-    while (!str.empty()) {
+    while (!str.empty())
+    {
         std::string_view::size_type space = str.find(' ');
         result.push_back(space == pos_end ? str.substr(0, str.size()) : str.substr(0, space));
         str.remove_prefix(std::min(str.find_first_not_of(' ', space), str.size()));
