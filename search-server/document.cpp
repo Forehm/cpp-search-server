5a4fba013d871da77cfea794164461c5bebb1892
Egor <112934317+Forehm@users.noreply.github.com>
2023-10-25 23:43:40 +0300

    Update document.cpp

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -3,25 +3,30 @@
 using namespace std;
 
 
-bool operator == (const Document& lhs, const Document& rhs) {
+bool operator == (const Document& lhs, const Document& rhs) 
+{
     return (lhs.id == rhs.id &&
         lhs.rating == rhs.rating &&
         abs(lhs.relevance - rhs.relevance) < 1e-6);
 }
 
-bool operator != (const Document& lhs, const Document& rhs) {
+bool operator != (const Document& lhs, const Document& rhs) 
+{
     return !(lhs == rhs);
 }
 
-bool operator < (const Document& lhs, const Document& rhs) {
+bool operator < (const Document& lhs, const Document& rhs) 
+{
 
-    if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+    if (abs(lhs.relevance - rhs.relevance) < 1e-6) 
+    {
         return lhs.rating < rhs.rating;
     }
     return lhs.relevance < rhs.relevance;
 }
 
-ostream& operator << (ostream& os, const Document& rhs) {
+ostream& operator << (ostream& os, const Document& rhs) 
+{
     os << "{ "s
         << "document_id = "s << rhs.id << ", "s
         << "relevance = "s << rhs.relevance << ", "s
